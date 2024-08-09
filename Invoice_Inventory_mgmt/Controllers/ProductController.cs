feb6b51ec5de6f8a83cf89bff5b58217bf5c5bc8
roiveredritesh <36503631+roiveredritesh@users.noreply.github.com>
2024-08-09 16:24:44 +0530

    Added get method to product controller

--- Invoice_Inventory_mgmt/Controllers/ProductController.cs
+++ Invoice_Inventory_mgmt/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Invoice_Inventory_mgmt.Service;
+﻿using Invoice_Inventory_mgmt.Common;
+using Invoice_Inventory_mgmt.Model;
+using Invoice_Inventory_mgmt.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,25 @@ namespace Invoice_Inventory_mgmt.Controllers
         public ProductController(IProductCategorySL productCategorySL)
         {
             _productCategorySL = productCategorySL;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            try
+            {
+                var productCategoryList = await _productCategorySL.GetProductCategoryList();
+                if (productCategoryList == null || !productCategoryList.Any())
+                {
+                    return NotFound(new CustomResponse<object> { Code = 404, Message = "Products not found", Data = null });
+                }
+                return Ok(new CustomResponse<List<ProductCategoryMaster>> { Code = 200, Message = "Success", Data = productCategoryList });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while fetching products: {ex}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while fetching products.");
+            }
         }
 
     }
